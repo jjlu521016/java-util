30f591bee331e952aa15e248b5b57d6a9e2414fd
jjlu521016 <jjlu521016@gmail.com>
2019-08-13 00:20:11 +0800

    add springboot+jedis

---
+++ redis-repeatsubmit/src/main/java/io/jjlu/redisrepeatsubmit/RedisRepeatsubmitApplication.java
@@ -0,0 +1,13 @@
+package io.jjlu.redisrepeatsubmit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisRepeatsubmitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisRepeatsubmitApplication.class, args);
+    }
+
+}
